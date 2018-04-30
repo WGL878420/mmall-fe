6c40eab233fc55e3785505d1671e0f7806c1dd51
chenjiezi <892774940@qq.com>
2018-04-30 22:39:40 +0800

    购模车模块

--- src/service/cart-service.js
+++ src/service/cart-service.js
@@ -2,7 +2,7 @@
  * @Author: chenjiezi 
  * @Date: 2018-04-15 16:20:01 
  * @Last Modified by: chenjiezi
- * @Last Modified time: 2018-04-30 13:38:41
+ * @Last Modified time: 2018-04-30 21:36:54
  */
  
 'use strict';
@@ -26,7 +26,73 @@ var _cart = {
             success : resolve,
             error   : reject
         });
-    }
+    },
+    // 获取购物车列表
+    getCartList : function(resolve, reject){
+        _mm.request({
+            url     : _mm.getServerUrl('/cart/list.do'),
+            success : resolve,
+            error   : reject
+        });
+    },
+    // 选择购物车商品
+    selectProduct : function(productId, resolve, reject){
+        _mm.request({
+            url     : _mm.getServerUrl('/cart/select.do'),
+            data    : {
+                productId : productId
+            },
+            success : resolve,
+            error   : reject
+        });
+    },
+    // 取消选择购物车商品
+    unselectProduct : function(productId, resolve, reject){
+        _mm.request({
+            url     : _mm.getServerUrl('/cart/un_select.do'),
+            data    : {
+                productId : productId
+            },
+            success : resolve,
+            error   : reject
+        });
+    },
+    // 选中全部商品
+    selectAllProduct : function(resolve, reject){
+        _mm.request({
+            url     : _mm.getServerUrl('/cart/select_all.do'),
+            success : resolve,
+            error   : reject
+        });
+    },
+    // 取消选中全部商品
+    unselectAllProduct : function(resolve, reject){
+        _mm.request({
+            url     : _mm.getServerUrl('/cart/un_select_all.do'),
+            success : resolve,
+            error   : reject
+        });
+    },
+    // 更新购物车商品数量
+    updateProduct : function(productInfo, resolve, reject){
+        _mm.request({
+            url     : _mm.getServerUrl('/cart/update.do'),
+            data    : productInfo,
+            success : resolve,
+            error   : reject
+        });
+    },
+    // 删除指定商品
+    deleteProduct : function(productIds, resolve, reject){
+        _mm.request({
+            url     : _mm.getServerUrl('/cart/delete_product.do'),
+            data    : {
+                productIds : productIds
+            },
+            success : resolve,
+            error   : reject
+        });
+    },
 }
 
 // 模块出口
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@
  * @Author: chenjiezi 
  * @Date: 2018-04-06 22:48:18 
  * @Last Modified by: chenjiezi
- * @Last Modified time: 2018-04-30 13:30:19
+ * @Last Modified time: 2018-04-30 21:34:55
  */
 
 var webpack             = require('webpack');
@@ -32,8 +32,9 @@ var config = {
     entry: {
         'common'                 : ['./src/page/common/index.js'],
         'index'                  : ['./src/page/index/index.js'],
-        'list'                  : ['./src/page/list/index.js'],
-        'detail'                  : ['./src/page/detail/index.js'],
+        'list'                   : ['./src/page/list/index.js'],
+        'detail'                 : ['./src/page/detail/index.js'],
+        'cart'                   : ['./src/page/cart/index.js'],
         'user-login'             : ['./src/page/user-login/index.js'],
         'user-register'          : ['./src/page/user-register/index.js'],
         'user-pass-reset'        : ['./src/page/user-pass-reset/index.js'],
@@ -83,6 +84,7 @@ var config = {
         new HtmlWebpackPlugin(getHtmlConfig('index', '首页')), 
         new HtmlWebpackPlugin(getHtmlConfig('list', '商品列表')), 
         new HtmlWebpackPlugin(getHtmlConfig('detail', '商品详情')), 
+        new HtmlWebpackPlugin(getHtmlConfig('cart', '购物车')),
         new HtmlWebpackPlugin(getHtmlConfig('user-login', '用户登录')), 
         new HtmlWebpackPlugin(getHtmlConfig('user-register', '用户注册')),
         new HtmlWebpackPlugin(getHtmlConfig('user-pass-reset', '找回密码')),
